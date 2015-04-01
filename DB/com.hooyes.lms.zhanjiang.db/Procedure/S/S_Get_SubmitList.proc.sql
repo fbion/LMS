91c44cd6149d243e58a68d670a3321df64479288
hooyes <me@hooyes.com>
2015-04-01 17:08:56 +0800

    -

--- DB/com.hooyes.lms.zhanjiang.db/Procedure/S/S_Get_SubmitList.proc.sql
+++ DB/com.hooyes.lms.zhanjiang.db/Procedure/S/S_Get_SubmitList.proc.sql
@@ -1,36 +1,40 @@
 ﻿-- DROP PROC [S_Get_SubmitList]
 GO
 -- =============================================
--- Version:     1.0.0.8
+-- Version:     1.0.1.1
 -- Author:		hooyes
 -- Create date: 2012-07-23
--- Update date: 2013-06-17
+-- Update date: 2015-03-23
 -- Desc:
 -- =============================================
-CREATE PROCEDURE [dbo].[S_Get_SubmitList]
-	@count int = 22
-AS
+CREATE PROCEDURE [dbo].[S_Get_SubmitList] @count INT = 100
+AS --UPDATE TOP ( 15 )
+    --        Member
+    --SET     Tag = 0
+    --WHERE   Tag = 100
+    --        AND DATEDIFF(DAY, regdate, GETDATE()) > 5
 
-	UPDATE TOP(5) Member
-	SET Tag = 0 
-	WHERE Tag = 100
-	and DATEDIFF(DAY,regdate,GETDATE())>5
-
-	SELECT 
-		TOP(@count)
-	     M.MID
-		,M.IDSN
-		,M.IDCard
-		,M.RegDate
-		,M.Year
-		,Score    = ISNULL(R.Score,0)
-		,Compulsory = ISNULL(R.Compulsory,8)
-		,Elective = ISNULL(R.Elective,18)
-		,Status = ISNULL(R.Status,0)
-	FROM Member M
-		inner join MemberCredit MC ON M.MID = MC.MID and MC.flag = 1 and MC.tag = 100
-		inner join Report R ON R.MID = M.MID and (R.Status = 0 OR R.Status is null)
-	WHERE DATEDIFF(HOUR,M.RegDate,GETDATE())>= 20
-		  and (M.ExpireDate is null or M.ExpireDate>=GETDATE())
-	ORDER BY NEWID()
-RETURN 0
+    SELECT TOP(@count) R.ID ,
+            M.MID ,
+            cardNumberType = 1 ,
+            cardNumber = M.IDCard ,
+            areaName = '湛江市' ,
+            [year] = LEFT(R.Year, 4) ,
+            classCode = P.Memo ,
+            classLearnTimeCount = CAST(ISNULL(R.Minutes, 0) AS DECIMAL(18, 0))  ,
+            score = ISNULL(R.Score, 0)
+    FROM    Member M
+            INNER JOIN Report R ON R.MID = M.MID
+                                   AND ( R.Status = 0
+                                         OR R.Status IS NULL
+                                       )
+                                   AND R.Minutes >= 1440
+                                   AND R.score >= 60
+            INNER JOIN dbo.Products P ON p.PID = R.Year
+    WHERE   DATEDIFF(HOUR, M.RegDate, GETDATE()) >= 20
+            AND ( M.ExpireDate IS NULL
+                  OR M.ExpireDate >= GETDATE()
+                )
+            AND M.MID >= 10000
+    ORDER BY NEWID()
+    RETURN 0
