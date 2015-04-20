6c6c7ad4cdd5f33027e8a0f417467e39ecf2e578
hooyes <me@hooyes.com>
2015-04-20 15:46:02 +0800

    `

---
+++ DB/com.hooyes.lms.cpa.db/Procedure/Get_MyPaper.proc.sql
@@ -0,0 +1,101 @@
+﻿-- =============================================
+-- Version:     1.0.1.0
+-- Author:		hooyes
+-- Create date: 2012-01-02
+-- Update date: 2015-02-14
+-- Desc: 从所学的课程中抽题
+-- =============================================
+CREATE PROCEDURE [dbo].[Get_MyPaper]
+    @MID INT = 0 ,
+    @Year INT = 0 ,
+    @Type INT = 0
+AS 
+    DECLARE @Question TABLE
+        (
+          [QID] [int] NOT NULL ,
+          [CName] [varchar](50) NOT NULL ,
+          [Subject] [nvarchar](300) NOT NULL ,
+          [A] [nvarchar](255) NULL ,
+          [B] [nvarchar](255) NULL ,
+          [C] [nvarchar](255) NULL ,
+          [D] [nvarchar](255) NULL ,
+          [Answer] [nvarchar](50) NOT NULL ,
+          [Score] [int] NOT NULL ,
+          [Cate] [int] NULL
+        )
+
+
+    DECLARE @Tags VARCHAR(100) ,
+        @Count INT = 0 ,
+        @Cate INT = 1    --- 目前 1,2,3
+
+
+    WHILE ( @Cate < 4 ) 
+        BEGIN
+            SET @Tags = '0' 
+            SET @Count = 0
+			
+            SELECT  @Tags = Tags ,
+                    @Count = [Count]
+            FROM    dbo.QuestionConfig
+            WHERE   [Year] = @Year
+                    AND [Cate] = @Cate
+            INSERT  INTO @Question
+                    ( [QID] ,
+                      [CName] ,
+                      [Subject] ,
+                      [A] ,
+                      [B] ,
+                      [C] ,
+                      [D] ,
+                      [Answer] ,
+                      [Score] ,
+                      [Cate]
+                    )
+                    SELECT TOP ( @Count )
+                            [QID] ,
+                            [CName] ,
+                            [Subject] ,
+                            [A] ,
+                            [B] ,
+                            [C] ,
+                            [D] ,
+                            [Answer] ,
+                            [Score] ,
+                            [Cate]
+                    FROM    Question
+                    WHERE   [Cate] = @Cate
+                            AND CName IN (
+                            SELECT  b.CName
+                            FROM    dbo.My_Courses a
+                                    INNER JOIN dbo.Courses b ON a.CID = b.CID
+                            WHERE   a.MID = @MID
+                                    AND b.Year = @Year 
+									AND a.[Status] = 1
+									)
+                    ORDER BY NEWID()
+            SET @Cate = @Cate + 1
+        END
+
+
+    SELECT  @Count = COUNT(1)
+    FROM    @Question
+
+	/* 取不到题，取默认题库*/
+    IF @Count > 0 
+        BEGIN
+            SELECT  *
+            FROM    @Question
+            ORDER BY Cate ASC
+        END 
+    ELSE 
+        BEGIN
+            EXECUTE [Get_MyPaper_Defaut] 
+                @MID = @MID ,
+                @Year = @Year ,
+                @Type = @Type
+        END   
+    
+
+ 
+    RETURN 0
---
+++ DB/com.hooyes.lms.cpa.db/Procedure/Task_EvaluteReport.sql
@@ -0,0 +1,27 @@
+﻿-- =============================================
+-- Version:     1.0.0.5
+-- Author:		hooyes
+-- Create date: 2012-04-13
+-- Update date: 2015-04-13
+-- Desc:
+-- =============================================
+CREATE PROCEDURE [dbo].[Task_EvaluteReport]
+    @MID INT ,
+    @PID INT = 2013 ,
+    @RegionCode INT = 0
+AS /* 10755 不用考试，分钟数够即可以结业 */
+    IF @RegionCode = 10755 
+        BEGIN
+            UPDATE  dbo.Report
+            SET     [Status] = 1 ,
+                    CommitDate = GETDATE() ,
+                    UpdateDate = GETDATE()
+            WHERE   [Minutes] >= 1125
+                    AND MID = @MID
+                    AND [Year] = @PID
+                    AND ( Status = 0
+                          OR Status IS NULL
+                        )
+        END   
+
+    RETURN 0
